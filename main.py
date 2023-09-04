4b40a547a3ffeeffac4c266c076cfd855eb9bb23
Omar <omarkhaled1752003@yahoo.com>
2023-09-04 18:42:55 +0300

    added timer, and count down functions

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import tkinter as tk
+import math
 
 # Constants
 
 BG_COLOR = "#176B87"
+
+# timer function
+def timer():
+    count_down(5 * 60)
+
+# count down function
+def count_down(time):
+    minutes = math.floor(time / 60)
+    seconds = time % 60
+    canvas.itemconfig(time_text, text=f"{minutes:02d}:{seconds:02d}")
+    window.after(1000, count_down, time - 1)
 
 # window setup
 
@@ -16,7 +28,7 @@ canvas = tk.Canvas(width=200, height=224, bg=BG_COLOR,
                    highlightbackground=BG_COLOR)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(102, 112, image=tomato_img)
-canvas.create_text(108, 130, text="00:00", font=(
+time_text = canvas.create_text(108, 130, text="00:00", font=(
     "courier", 30, "bold"), fill="white")
 canvas.grid(column=1, row=1)
 
@@ -28,7 +40,7 @@ tk.Label(text="✔", fg="#1A5D1A", bg=BG_COLOR, font=("courier", 30)).grid(colum
 
 # Buttons
 
-start_button = tk.Button(text="Start", font=(
+start_button = tk.Button(text="Start", command=timer, font=(
     "arial", 10), highlightthickness=0)
 start_button.grid(column=0, row=2)
 
