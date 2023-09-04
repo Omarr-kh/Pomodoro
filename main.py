608046d067dd595f2d022e4061aacd7dbbef9d3f
Omar <omarkhaled1752003@yahoo.com>
2023-09-04 15:11:53 +0300

    added the canvas

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import tkinter as tk
 # window setup
 window = tk.Tk()
 window.title("Pomodoro")
-window.config(width=400, height=400)
+window.config(padx=100, pady=50)
 
 # Canvas
+canvas = tk.Canvas(width=200, height=224)
+tomato_img = tk.PhotoImage(file="tomato.png")
+canvas.create_image(102, 112, image=tomato_img)
+canvas.pack()
 
 window.mainloop()
